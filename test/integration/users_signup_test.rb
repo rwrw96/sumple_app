85409819d2b781b0022e80dd9f238204b0465369
Ubuntu <ubuntu@ip-172-31-1-160.us-east-2.compute.internal>
2021-03-16 03:34:04 +0000

    ssl

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    follow_redirect!
+    # follow_redirect!
     assert_template 'users/new'
   end
 end
